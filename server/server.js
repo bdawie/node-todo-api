40b58b7d4f6dfbb324a1c6d4e1eb417042ca4e71
bdawie <ibrahimbdawie@gmail.com>
2017-07-11 00:51:39 +0300

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');  
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -27,7 +27,7 @@ app.get('/todos',(req,res)=>{
     res.status(400).send(e);
   });
 });
-
+  
 app.get('/todos/:id',(req,res)=>{
   var id = req.params.id;
   if(!ObjectID.isValid(id)){
