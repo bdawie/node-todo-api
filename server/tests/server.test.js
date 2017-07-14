ef6d17de9580aee6e51125dff8639dc53ed472e8
bdawie <ibrahimbdawie@gmail.com>
2017-07-14 19:50:46 +0300

    Add PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -93,3 +93,37 @@ describe('POST /todos',()=>{
         .end(done);
     });
   });
+  describe('DELETE /todos:/id',()=>{
+    it('should delete doc by id',(done)=>{
+      var hexId= todos[0]._id.toHexString();
+      request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res)=>{
+          expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end((err,res)=>{
+          if(err){
+            return done(err);
+          }
+          Todo.findById(hexId).then((todo)=>{
+            expect(todo).toNotExist();
+            done();
+          }).catch((e)=> done(e);)
+        });
+    }); 
+    it('should return 404 if todo not found',(done)=>{
+      var id = new ObjectID().toHexString();
+      request(app)
+        .delete(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+    it('should return 404 for non-object ids',(done)=>{
+      request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done);
+    });
+
+  });
